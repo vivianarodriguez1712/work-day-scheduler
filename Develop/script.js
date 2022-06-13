7fd1e1adf2969ac44401b7ee63c4a357d6086c1a
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-12 23:39:21 -0400

    finished completing functions in js

--- Develop/script.js
+++ Develop/script.js
@@ -1,45 +1,152 @@
-document.getElementById("todaysDate").textContent = moment().format('dddd, MMM Do YYYY');
+var hour9 = 9;
+var hour10 = 10;
+var hour11 = 11;
+var hour12 = 12;
+var hour1 = 13;
+var hour2 = 14;
+var hour3 = 15;
+var hour4 = 16;
+var hour5 = 17;
 
-function trackTime() {
-    var currentTime = moment().hour();
+var displayTime = document.querySelector('#todaysDate');
+var currentTime = moment();
 
-    $(".time-block").each(function () {
-        var timeBlock = parseInt($(this).attr("id").split("hour")[1])
+displayTime.textContent = currentTime.format('dddd, MMMM Do');
 
-        if (timeBlock < currentTime) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
+$('.saveBtn').on('click', function () {
+  var value = $(this).siblings('.description').val();
+  console.log(value);
+  var time = $(this).parent().attr('id');
+  console.log(time);
 
-        else if (timeBlock === currentTime) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
+  localStorage.setItem(time, value);
+});
 
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
+$('#9am-row .description').val(
+  localStorage.getItem('9am-row')
+);
 
-        }
-    })
+// 10am
+$('#10am-row .description').val(
+  localStorage.getItem('10am-row')
+);
+
+// 11am
+$('#11am-row .description').val(
+  localStorage.getItem('11am-row')
+);
+
+// 12pm
+$('#12pm-row .description').val(
+  localStorage.getItem('12pm-row')
+);
+
+// 1pm
+$('#1pm-row .description').val(
+  localStorage.getItem('1pm-row')
+);
+
+// 2pm
+$('#2pm-row .description').val(
+  localStorage.getItem('2pm-row')
+);
+
+// 3pm
+$('#3pm-row .description').val(
+  localStorage.getItem('3pm-row')
+);
+
+// 4pm
+$('#4pm-row .description').val(
+  localStorage.getItem('4pm-row')
+);
+
+// 5pm
+$('#5pm-row .description').val(
+  localStorage.getItem('5pm-row')
+);
+
+console.log('H is for Hour');
+var currentHour = moment().format('H');
+
+// 9am
+if (currentHour < hour9) {
+  $('#9am-text').addClass('future');
+} else if (currentHour > hour9) {
+  $('#9am-text').addClass('past');
+} else if ((currentHour = hour9)) {
+  $('#9am-text').addClass('present');
 }
 
-function saveTask(event) {
-   var taskEvent = event.target.getAttribute("type")
-   console.log(taskEvent)
+// 10am
+if (currentHour < hour10) {
+  $('#10am-text').addClass('future');
+} else if (currentHour > hour10) {
+  $('#10am-text').addClass('past');
+} else if ((currentHour = hour10)) {
+  $('#10am-text').addClass('present');
+}
 
-   var key = Math.random();
-   var value = 
+// 11am
+if (currentHour < hour11) {
+  $('#11am-text').addClass('future');
+} else if (currentHour > hour11) {
+  $('#11am-text').addClass('past');
+} else if ((currentHour = hour11)) {
+  $('#11am-text').addClass('present');
+}
 
+// 12pm
+if (currentHour < hour12) {
+  $('#12pm-text').addClass('future');
+} else if (currentHour > hour12) {
+  $('#12pm-text').addClass('past');
+} else if ((currentHour = hour12)) {
+  $('#12pm-text').addClass('present');
+}
 
-trackTime();
+// 1pm
+if (currentHour < hour1) {
+  $('#1pm-text').addClass('future');
+} else if (currentHour > hour1) {
+  $('#1pm-text').addClass('past');
+} else if ((currentHour = hour1)) {
+  $('#1pm-text').addClass('present');
+}
 
+// 2pm
+if (currentHour < hour2) {
+  $('#2pm-text').addClass('future');
+} else if (currentHour > hour2) {
+  $('#2pm-text').addClass('past');
+} else if ((currentHour = hour2)) {
+  $('#2pm-text').addClass('present');
+}
 
+// 3pm
+if (currentHour < hour3) {
+  $('#3pm-text').addClass('future');
+} else if (currentHour > hour3) {
+  $('#3pm-text').addClass('past');
+} else if ((currentHour = hour3)) {
+  $('#3pm-text').addClass('present');
+}
 
+// 4pm
+if (currentHour < hour4) {
+  $('#4pm-text').addClass('future');
+} else if (currentHour > hour4) {
+  $('#4pm-text').addClass('past');
+} else if ((currentHour = hour4)) {
+  $('#4pm-text').addClass('present');
+}
 
-
-
+// 5pm
+if (currentHour < hour5) {
+  $('#5pm-text').addClass('future');
+} else if (currentHour > hour5) {
+  $('#5pm-text').addClass('past');
+} else if ((currentHour = hour5)) {
+  $('#5pm-text').addClass('present');
+}
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Work Day Scheduler Starter Code
+
+## Purpose 
+Creating a planner that allows you to type what you want and save it. You can go in and edit it or delete it. The planner also always changes the date to the current date.
+
+## Built with
+* HTML
+* CSS
+* JS
+
+# Website
+https://vivianarodriguez1712.github.io/work-day-scheduler/
+
+## Contribution
+Made with ❤️ by Viviana Rodriguez
